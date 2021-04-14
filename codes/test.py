e713b169e0e9b1130afa5e7f4b40838469f0ef0a
Zhou Yang <zhou.yang.19@ucl.ac.uk>
2021-04-14 14:40:06 +0800

    test(biasRV): test repair_with_majority_rule

--- codes/test.py
+++ codes/test.py
@@ -1,4 +1,5 @@
 from sentiment_analysis import check_property_1
+from sentiment_analysis import repair_with_majority_rule
 
 def test_check_property_1():
     # generate test case and oracle
@@ -16,5 +17,11 @@ def test_check_property_1():
     assert not check_property_1(1, [0] * N, [1] * (N + 1), N)
 
 
+def test_repair_with_majority_rule():
+
+    assert repair_with_majority_rule([0,0,0]) == 0
+    assert repair_with_majority_rule([1,1,1]) == 1
+    assert repair_with_majority_rule([1,1,0]) == 1
+    assert repair_with_majority_rule([0,1,0]) == 0
 
     
