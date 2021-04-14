278be81c5881c187904b73570df38da24f4158d3
Zhou Yang <zhou.yang.19@ucl.ac.uk>
2021-04-14 10:01:44 +0800

    feat(): script to run the BiasRV tool

--- codes/verify_sa.py
+++ codes/verify_sa.py
@@ -1,5 +1,6 @@
+import pandas as pd
 from sentiment_analysis import SentimentAnalysis
-
+import time
 
 if __name__=="__main__":
 
@@ -15,5 +16,39 @@ if __name__=="__main__":
     
     text = "The Great Dictator is a beyondexcellent film. Charlie Chaplin succeeds in being both extremely funny and witty and yet at the same time provides a strong statement in his satire against fascism. The antiNazi speech by Chaplin at the end, with its values, is one of filmdom's great moments. Throughout this movie, I sensed there was some higher form of intelligence, beyond genuinely intelligent filmmaking, at work."
 
-    result = sa_system.predict(text, True)
-    print(result)
+
+    df = pd.read_csv("../asset/imdb/test.csv", names=["label", "sentence"], sep="\t")
+    count = 0
+    correct_count_RV = 0
+    correct_count = 0
+    not_best_case = 0
+
+    start = time.time()
+    for index, row in df.iterrows():
+        count += 1
+        if count % 1000 == 0:
+            pass
+        label = row["label"]
+        text = row["sentence"]
+        final_result, confidence, results = sa_system.predict(text, True)
+
+        # compute SA system accuracy
+        if (int(label) == final_result):
+            correct_count_RV += 1
+
+        if confidence != 1:
+            print((int(label) == final_result))
+            not_best_case += 1
+        
+        final_result, confidence, results = sa_system.predict(text, False)
+        if (int(label) == final_result):
+            correct_count += 1
+
+    
+    print("Accuracy after RV: ", 1.0 * correct_count_RV / count)
+    print("Accuracy without RV: ", 1.0 * correct_count / count)
+    print("Potential bias rate: ", 1.0 * not_best_case / count)
+
+    end = time.time()
+
+    print("Time consumed: ", end - start)
